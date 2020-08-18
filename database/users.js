b5d94cd2dc2ad962645c9ebd47085ad890e3ece5
MarcusV <marcusviniciuslma@gmail.com>
2020-08-18 18:05:22 -0300

    Criando interface do DB com lowdb e criando funções básicas de acesso ao DB para USER e MOVIE

---
+++ database/users.js
@@ -0,0 +1,55 @@
+const db = require("./config");
+const { all } = require("../routes");
+
+function createUser(userInfo) {
+  const userExists = findUser({ email: userInfo.email });
+
+  if (userExists) {
+    return null;
+  } else {
+    const createdUser = db
+      .get("profiles")
+      .push(userInfo)
+      .last()
+      .assign({ id: Date.now().toString() })
+      .write();
+
+    return createdUser;
+  }
+}
+
+function getUser(id) {
+  const user = db.get("profiles").find({ id }).value();
+
+  return user;
+}
+
+function findUser(searchParams) {
+  if (searchParams) {
+    const user = db.get("profiles").find(searchParams).value();
+
+    return user;
+  } else {
+    const allUsers = db.get("profiles").value();
+
+    return allUsers;
+  }
+}
+
+function updateUser(userInfo) {
+  const updatedUser = db
+    .get("profiles")
+    .find({ id: userInfo.id })
+    .assign(userInfo)
+    .value()
+    .write();
+
+  return updatedUser;
+}
+
+module.exports = {
+  createUser,
+  getUser,
+  findUser,
+  updateUser,
+};
