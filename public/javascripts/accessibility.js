0b7d77654c5c3647664594a82fce41e8d08140b1
DanielAraujoSouza <web.daniel.araujo@gmail.com>
2020-08-27 01:23:06 -0300

    Melhorias na funcionalidade
    - Salvar tamanho da fonte na local Storage API;
    - Limite da fonte min 16px e max 24;
    - Aumento do passo de alteração da fonte de 1 para 2px.

--- public/javascripts/accessibility.js
+++ public/javascripts/accessibility.js
@@ -1,43 +1,41 @@
-(function () {
+(() => {
+  // Recupera o tema salvo
   if (localStorage.getItem('theme') === 'high-contrast') {
     setTheme('high-contrast');
   } else {
     setTheme('common');
   }
+  // Recupera o tamanho da fonte salva
+  setFontSize(parseInt(localStorage.getItem('fontSize')));
 })();
+
+// Troca o tema
+$("a.contrast").click(() => {
+  if (localStorage.getItem('theme') === 'high-contrast') {
+    setTheme('common');
+  } else {
+    setTheme('high-contrast');
+  }
+});
+
+// Aumenta o tamanho da fonte
+$("a.font-size-increase").click(() => {
+    setFontSize(parseInt($(".accessibility").css('font-size')) + 2);
+});
+
+// Diminui o tamanho da fonte
+$("a.font-size-decrease").click(() => {
+    setFontSize(parseInt($(".accessibility").css('font-size')) - 2);
+});
 
 function setTheme(themeName) {
   localStorage.setItem('theme', themeName);
   document.documentElement.className = themeName;
 }
 
-// Troca o tema
-$("a.contrast").click(function()
-  {
-    if (localStorage.getItem('theme') === 'high-contrast') {
-      setTheme('common');
-    } else {
-      setTheme('high-contrast');
-    }
+function setFontSize(fontSize) {
+  if (fontSize >= 16 && fontSize <= 24) {
+    $(".accessibility").css("fontSize", fontSize);
+    localStorage.setItem('fontSize', fontSize);
   }
-);
-
-// Aumenta o tamanho da fonte
-$("a.font-size-increase").click(function()
-  {
-    var elemento = $(".accessibility");
-    var fonte = parseInt(elemento.css('font-size'));
-    fonte++;
-    elemento.css("fontSize", fonte);
-  }
-);
-
-// Diminui o tamanho da fonte
-$("a.font-size-decrease").click(function()
-  {
-    var elemento = $(".accessibility");
-    var fonte = parseInt(elemento.css('font-size'));
-    fonte--;
-    elemento.css("fontSize", fonte);
-  }
-);
+}
