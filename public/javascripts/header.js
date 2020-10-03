b30be3546dd80a7598b3471e5a93c67473f2649d
DanielAraujoSouza <web.daniel.araujo@gmail.com>
2020-10-03 15:54:02 -0300

    Atualização da Navbar
    - Remoção de funções descontinuadas;
    - Animação da barra de navegação;
    - Fixação da barra para alto contraste.

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,10 +1,24 @@
 const headerSearchbar = document.getElementById("header-searchbar");
+const navbar = document.getElementById("navbar");
+const searchbarIcon = document.getElementById("searchbar-icon");
 
 if(headerSearchbar) {
-  headerSearchbar.addEventListener("keyup", function (e) {
-    var key = e.which || e.keyCode;
-    if (key == 13) {
+  headerSearchbar.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
       window.location.href = `/title/search?title=${this.value}`;
     }
   });
 }
+
+// Animação da Barra
+let prevScrollpos = window.pageYOffset;
+window.addEventListener("scroll", (e) =>{
+  if (localStorage.getItem('theme') !== 'high-contrast') {
+    navbar.style.top = prevScrollpos > window.pageYOffset ? "0" : "-50px";
+    prevScrollpos = window.pageYOffset;
+  }
+});
+
+navbar.addEventListener("mouseenter", (e) =>{
+  navbar.style.top = "0";
+});
